ee2fbd23a1e9808105a84b1ad88af25af50ce29b
chaewon-io <0101angela@naver.com>
2024-02-27 15:29:28 +0900

    Init: 초기 세팅

---
+++ src/main/java/com/senity/waved/WavedApplication.java
@@ -0,0 +1,13 @@
+package com.senity.waved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WavedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WavedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senity/waved/WavedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senity.waved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WavedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
