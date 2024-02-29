97c4f278a668be9c3a3637c48c78b88dc5900210
sdw504512 <sdw504512@gmail.com>
2024-02-29 15:52:35 +0900

    Feat: 로그인 Redis 적용

--- src/main/java/com/senity/waved/base/security/OAuth2MemberSuccessHandler.java
+++ src/main/java/com/senity/waved/base/security/OAuth2MemberSuccessHandler.java
@@ -2,11 +2,14 @@ package com.senity.waved.base.security;
 
 import com.senity.waved.base.jwt.TokenDto;
 import com.senity.waved.base.jwt.TokenProvider;
+import com.senity.waved.base.redis.Redis;
+import com.senity.waved.base.redis.RedisRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -19,12 +22,17 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
     @Value("${custom.site.baseUrl}")
     private String REDIRECT_URI;
     private final TokenProvider tokenProvider;
+    private final RedisRepository redisRepository;
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        String userEmail = authentication.getName();
+
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String userEmail = (String) oAuth2User.getAttribute("email");
         TokenDto token = tokenProvider.createToken(userEmail);
+
+        redisRepository.save(new Redis(userEmail, token.getRefreshToken()));
 
         String url = makeRedirectUrl(token.getAccessToken());
         getRedirectStrategy().sendRedirect(request, response, url);
