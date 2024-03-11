b2a7d50f240b7b740b8148db41b94d477da648f9
chaewon-io <0101angela@naver.com>
2024-03-12 04:20:56 +0900

    Feat: 내 인증 현황 조회

--- src/main/java/com/senity/waved/domain/myChallenge/service/MyChallengeService.java
+++ src/main/java/com/senity/waved/domain/myChallenge/service/MyChallengeService.java
@@ -1,5 +1,8 @@
 package com.senity.waved.domain.myChallenge.service;
 
+import com.senity.waved.domain.myChallenge.entity.MyChallenge;
+
 public interface MyChallengeService {
+    MyChallenge findMyChallengeById(Long myChallengeId);
 
 }
