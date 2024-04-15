6c0bf583a7b6d5c3051e72cb2f5848377e1617c7
sdw504512 <sdw504512@gmail.com>
2024-04-15 14:46:19 +0900

    Test: git commit date 시간 확인

--- src/main/java/com/senity/waved/domain/verification/service/GithubService.java
+++ src/main/java/com/senity/waved/domain/verification/service/GithubService.java
@@ -1,14 +1,17 @@
 package com.senity.waved.domain.verification.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.kohsuke.github.GHCommit;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -17,7 +20,10 @@ public class GithubService {
 
     public boolean hasCommitsToday(String githubId, String token) throws IOException {
         GHCommit commits = githubApi.getCommits(githubId, token);
-        LocalDate commitDate = commits.getCommitDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        return commitDate.equals(LocalDate.now());
+        ZonedDateTime commitDate = commits.getCommitDate().toInstant().atZone(ZoneId.of("GMT")).plusHours(9).truncatedTo(ChronoUnit.DAYS);
+        // if (latestGroup.getStartDate().plusHours(9).equals(ZonedDateTime.now(ZoneId.of("GMT")).plusHours(9).truncatedTo(ChronoUnit.DAYS))
+        log.error("----------------------commitDate : ", commitDate);
+        log.error("----------------------today      : ", ZonedDateTime.now(ZoneId.of("GMT")).plusHours(9).truncatedTo(ChronoUnit.DAYS));
+        return commitDate.equals(ZonedDateTime.now(ZoneId.of("GMT")).plusHours(9).truncatedTo(ChronoUnit.DAYS));
     }
 }
